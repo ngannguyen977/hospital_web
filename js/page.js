2798aa059c63f4f2a9124391b409d68f2b20242d
Moc Huong Garden <ngannguyen977@gmail.com>
2020-02-24 17:05:16 +0700

    update page

--- js/page.js
+++ js/page.js
@@ -1,20 +1,34 @@
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-  interval: false
-});
+$('#carousel-example').on('slide.bs.carousel', function (e) {
 
-// for every slide in carousel, copy the next slide's item in the slide.
-// Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-    $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
+      /*
+          CC 2.0 License Iatek LLC 2018
+          Attribution required
+      */
+      var $e = $(e.relatedTarget);
+      var idx = $e.index();
+      var itemsPerSlide = 5;
+      var totalItems = $('.carousel-item').length;
+      
+      if (idx >= totalItems-(itemsPerSlide-1)) {
+          var it = itemsPerSlide - (totalItems - idx);
+          for (var i=0; i<it; i++) {
+              // append slides to end
+              if (e.direction=="left") {
+                  $('.carousel-item').eq(i).appendTo('.carousel-inner');
+              }
+              else {
+                  $('.carousel-item').eq(0).appendTo('.carousel-inner');
+              }
+          }
+      }
+  });
+jQuery(document).ready(function() {
+  jQuery('.play i').click(function(event){
+     event.preventDefault();
+    //var url = $(this).html(); //this will not work  
+     $(".js-video").append('<iframe width="940" height="529" src="https://www.youtube.com/embed/ZsCL47eqgrs?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+    $(this).hide();
+    //$('video-poster').css('z-index','-1');
+    
+  });
 });
